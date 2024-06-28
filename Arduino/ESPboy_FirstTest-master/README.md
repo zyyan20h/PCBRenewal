fad3a0d1434033594277c9b6a3dbce85683c4469
jiashengli <jsli@umd.edu>
2024-06-28 17:07:23 -0400

    Added Arduino testing files. Espboy works

---
+++ Arduino/ESPboy_FirstTest-master/README.md
@@ -0,0 +1,100 @@
+# Try "First test" to check your ESPboy after assembling
+www.espboy.com project
+
+There are 3 versions:
+
+1. ESPboy_FristTest_AdafruitLibs - does not use custom ESPboy libs and uses only standard [Adafruit libraries (slow and poor, but simple)](https://github.com/adafruit/Adafruit-ST7735-Library) to control ESPboy hardware (LED, buttons, sound, display)
+
+2. ESPboy_FirstTest_ESPboyLibs_TFTeSPI -- *uses ESPboy libs* and [TFT_eSPI (fastest)](https://github.com/Bodmer/TFT_eSPI?ysclid=ln67tav62z348840877) graphics library for display (be carefull, to force it work properly with ESPboy's display you have to edit parameters in "User_Setup.h" file in TFT_eSPI library folder according [to this](https://github.com/ESPboy-edu/ESPboy_ArduinoIDE_Libraries/blob/master/TFT_eSPI-master/User_Setup.h) )
+
+3. ESPboy_FirstTest_ESPboyLibs_LovyanGFX -- *uses ESPboy libs* and [LovyanGFX (most advanced)](https://github.com/lovyan03/LovyanGFX) graphics library for display
+
+4. ESPboy_ArdublockFirstTest -- uses [Ardublock.ru v3](http://ardublock.ru/3/)
+
+# To start work with ESPboy
+
+1. Install Arduino IDE legacy 1.8.n recommended (not v2.n IDE) https://www.arduino.cc/en/Main/Software
+
+2. Install the WeMos D1 mini ESP866 board to your Arduino IDE https://www.instructables.com/Wemos-ESP8266-Getting-Started-Guide-Wemos-101/
+
+3. Install all the libs for ESPboy from here https://github.com/ESPboy-edu/ESPboy_ArduinoIDE_Libraries
+
+4. Select the "LOLIN(WEMOS) D1 R2 & mini" board in the Arduino IDE and compile this code
+
+5. Test ESPboy hardware:
+- Power on
+- 8 buttons
+- Display TFT and its backlit
+- RGB led
+- Buzzer/Speaker
+
+6. For better experience you have to istall to your Arduino IDE ["Little FS file uploader plugin"](https://github.com/earlephilhower/arduino-esp8266littlefs-plugin) and ["ESP exception decoder plugin"](https://github.com/me-no-dev/EspExceptionDecoder)
+
+7. If you want to have professinal development tool much more powerfull than standard Arduino IDE, learn ["VS Code" + "PlatformIO"](https://circuitsgeek.com/guides-and-how-to/programming-esp8266-using-vs-code-and-platformio/)
+
+8. [Use recommended Arduino IDE settings](https://github.com/ESPboy-edu/ESPboy_FirstTest/blob/master/settings.png)
+
+![Arduino IDE settings](settings.png)
+
+
+
+# Try "First test" to start programming
+
+1. Use this example to start coding with ESPboy
+
+2. To init ESPboy hardware use begin() of [ESPboy_Init class](https://github.com/ESPboy-edu/ESPboy_Classes)
+
+3. For doing graphics use methods of [TFT_eSPI class](https://github.com/Bodmer/TFT_eSPI) or [adafruit ST7735 lib](https://github.com/adafruit/Adafruit-ST7735-Library) or [LovyanGFX lib](https://github.com/lovyan03/LovyanGFX) according to your GFX-way choice
+
+4. For buttons use getkeys() of ESPboyInit and keypressed&PAD_LEFT, keypressed&PAD_RIGHT and so on to understand what key is exactly pressed
+
+5. For LED use ESPboy_LED class https://github.com/ESPboy-edu/ESPboy_Classes or NeoPixel https://github.com/adafruit/Adafruit_NeoPixel
+
+6. For sound use playTone(frequency, duration), playTone (frequency) and noPlayTone() methods of ESPboy_Init class https://github.com/ESPboy-edu/ESPboy_Classes 
+
+7. To control the LCD backlit use Adafruit MCP4725 library methods https://github.com/adafruit/Adafruit_MCP4725 (not supported in ESPboy easy DIY versions)
+
+8. To upload data (files) to internal flash file system use Arduino IDE plugins: 
+
+a) [Little FS](https://github.com/earlephilhower/arduino-esp8266littlefs-plugin)
+
+b) [SPIFFS](https://github.com/esp8266/arduino-esp8266fs-plugin)
+
+9. To upload to ESPboy already compiled ".bin" files, use [Flash Download Tools](https://github.com/SequoiaSan/Guide-How-To-Upload-bin-to-ESP8266-ESP32)
+
+10. You can try [alternate ESPboy lib, made by Steph](https://www.arduino.cc/reference/en/libraries/espboy/) and [instructions](https://m1cr0lab-espboy.github.io/ESPboy)
+
+11. Use ESP8266 Arduino Core documentation https://arduino-esp8266.readthedocs.io/en/latest/index.html
+
+12. Use [WebAppStore](https://espboy.m1cr0lab.com/demo/appstore/) and WiFiAppStore directly from the device (first install "AppStore2 (OTA2)" from the WebAppStore)
+
+13. Ask questions in [ESPboy forum](https://community.arduboy.com) and ESPboy [discord chat](https://discord.gg/kMWhY2x)
+
+14. Have FUN! :)
+
+Regards,
+Roman
+//
+
+
+MIT License
+
+Copyright (c) 2020 ESPboy
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ Arduino/ESPboy_GameBoy/ESPboy_GameBoy.ino
@@ -0,0 +1,530 @@
+/*
+GameBoy emulator for ESPboy.
+
+You are able to try Nintendo retro games like SuperMario, Zelda, Pokemon, etc! 
+Pressing both side buttons, you can adjust view port (original GB screen resolution is 160х144 but it's only 128x128 window visible on ESPboy display).
+
+Peanut-GB core by deltabeard
+https://github.com/deltabeard/Peanut-GBhttps://github.com/deltabeard/Peanut-GB
+
+https://hackaday.io/project/164830-espboy-games-iot-stem-for-education-funhttps://hackaday.io/project/164830-espboy-games-iot-stem-for-education-fun
+
+MIT license
+*/
+
+#pragma GCC optimize ("-O3")
+#pragma GCC push_options
+
+#include "Arduino.h"
+#include <mmu_iram.h>
+
+#include "lib/ESPboyInit.h"
+#include "lib/ESPboyInit.cpp"
+//#include "lib/ESPboyTerminalGUI.h"
+//#include "lib/ESPboyTerminalGUI.cpp"
+//#include "lib/ESPboyOTA2.h"
+//#include "lib/ESPboyOTA2.cpp"
+
+#include <ESP_EEPROM.h>
+#include "sound.h"
+#include "peanut_gb.c"
+#include "LittleFS.h"
+#include "nbSPI.h"
+#include <sigma_delta.h>
+
+ESPboyInit myESPboy;
+//ESPboyTerminalGUI *terminalGUIobj = NULL;
+//ESPboyOTA2 *OTA2obj = NULL;
+
+
+//------------------------------------sprite_1-----------------------------------------------------------sprite_2-------------------------------------------------------------background----------
+//  const uint16_t palette0[] = { 0x79DF, 0x2D7E, 0x6D2B, 0x6308, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x7FFF, 0x3FE6, 0x0200, 0x0000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x6ACE, 0x279D, 0xE46B, 0x613A }; // PeanutGB
+//  const uint16_t palette1[] = { 0x7FFF, 0x7EAC, 0x40C0, 0x0000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x7FFF, 0x3FE6, 0x0200, 0x0000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x6ACE, 0x279D, 0xE46B, 0x613A }; // OBJ0
+  const uint16_t palette2[] = { 0x7FFF, 0x3FE6, 0x0200, 0x0000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x7FFF, 0x3FE6, 0x0200, 0x0000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x7FFF, 0x3FE6, 0x0200, 0x0000 }; // OBJ1
+  const uint16_t palette3[] = { 0x7FFF, 0x7EAC, 0x40C0, 0x0000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x7FFF, 0x7EAC, 0x40C0, 0x0000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x7FFF, 0x7EAC, 0x40C0, 0x0000 }; // BG
+  const uint16_t palette4[] = { 0x6ACE, 0x279D, 0xE46B, 0x613A, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x6ACE, 0x279D, 0xE46B, 0x613A, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x6ACE, 0x279D, 0xE46B, 0x613A }; //nostalgia
+//  const uint16_t palette5[] = { 0x5685, 0x8F9D, 0x6843, 0x6541, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x5685, 0x8F9D, 0x6843, 0x6541, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x5685, 0x8F9D, 0x6843, 0x6541 }; //greeny
+  const uint16_t palette6[] = { 0x16F6, 0xC9D3, 0xC091, 0x8041, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x16F6, 0xC9D3, 0xC091, 0x8041, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x16F6, 0xC9D3, 0xC091, 0x8041 }; //reddy
+//  const uint16_t palette7[] = { 0x79DF, 0x2D7E, 0x6D2B, 0x6308, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x79DF, 0x2D7E, 0x6D2B, 0x6308, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x79DF, 0x2D7E, 0x6D2B, 0x6308 }; //retro lcd
+//  const uint16_t palette8[] = { 0x1F87, 0x795C, 0x7C72, 0x6959, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x1F87, 0x795C, 0x7C72, 0x6959, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x1F87, 0x795C, 0x7C72, 0x6959 };  //WISH GB
+  const uint16_t palette9[] = { 0xDDF7, 0xB7C5, 0xCE52, 0x6308, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xDDF7, 0xB7C5, 0xCE52, 0x6308, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xDDF7, 0xB7C5, 0xCE52, 0x6308 };  //HOLLOW
+//  const uint16_t palette10[] ={ 0x9B9F, 0xB705, 0xF102, 0x4A01, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x9B9F, 0xB705, 0xF102, 0x4A01, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x9B9F, 0xB705, 0xF102, 0x4A01 };  //BLK AQU4
+  const uint16_t palette11[] ={ 0x49CD, 0x099B, 0x0549, 0x4320, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x49CD, 0x099B, 0x0549, 0x4320, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x49CD, 0x099B, 0x0549, 0x4320 };  //GOLD GB
+//  const uint16_t palette12[] ={ 0x719F, 0x523D, 0xEE42, 0x0629, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x719F, 0x523D, 0xEE42, 0x0629, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x719F, 0x523D, 0xEE42, 0x0629 };  //NYMPH GB
+//  const uint16_t palette13[] ={ 0x16F6, 0xC9D3, 0xC091, 0x8041, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x16F6, 0xC9D3, 0xC091, 0x8041, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x5685, 0x8F9D, 0x6843, 0x6541 };  //BOOTLEG BY PIXELSHIFT
+//  const uint16_t palette14[] ={ 0x49CD, 0x099B, 0x0549, 0x4320, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x49CD, 0x099B, 0x0549, 0x4320, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x6ACE, 0x279D, 0xE46B, 0x613A };  //nostalgia+GOLD GB
+
+  const uint16_t *paletteN[] = {/*palette0, palette1,*/ palette2, palette3, palette4, /*palette5,*/ palette6, /*palette7, palette8,*/ palette9, /*palette10, */palette11/*, palette12, palette13, palette14*/};
+  uint16_t *paletteNN;
+  
+
+//#include "GAMES/rom_1.h"  //test rom
+//#define APP_MARKER 0xCA01
+//#include "GAMES/rom_2.h"  //super mario land
+//#define APP_MARKER 0xCA02
+//#include "GAMES/rom_3.h"  //tetris
+//#define APP_MARKER 0xCA03
+//#include "GAMES/rom_4.h"  //lemmings
+//#define APP_MARKER 0xCA04
+//#include "GAMES/rom_5.h"  //kirby's dream land
+//#define APP_MARKER 0xCA05
+//#include "GAMES/rom_6.h"  //mega man
+//#define APP_MARKER 0xCA06
+//#include "GAMES/rom_7.h"  //zelda
+//#define APP_MARKER 0xCA07
+//#include "GAMES/rom_8.h"  //prince of persia
+//#define APP_MARKER 0xCA08
+//#include "GAMES/rom_9.h"  //contra
+//#define APP_MARKER 0xCA09
+//#include "GAMES/rom_10.h" //Felix the cat
+//#define APP_MARKER 0xCA10
+//#include "GAMES/rom_11.h" //Pokemon
+//#define APP_MARKER 0xCA11
+//#include "GAMES/rom_12.h" //Castelian
+//#define APP_MARKER 0xCA12
+//#include "GAMES/rom_13.h" //Castelvania
+//#define APP_MARKER 0xCA13
+//#include "GAMES/rom_14.h" //Donkey Kong Land
+//#define APP_MARKER 0xCA14
+//#include "GAMES/rom_15.h" //Double dragon
+//#define APP_MARKER 0xCA15
+//#include "GAMES/rom_16.h" //R-type
+//#define APP_MARKER 0xCA16
+//#include "GAMES/rom_17.h" //Mega man III
+//#define APP_MARKER 0xCA17
+//#include "GAMES/rom_18.h" //R-type II
+//#define APP_MARKER 0xCA18
+//#include "GAMES/rom_19.h" //nemezis
+//#define APP_MARKER 0xCA19
+//#include "GAMES/rom_20.h" //ninja gaiden shadow
+//#define APP_MARKER 0xCA20
+//#include "GAMES/rom_21.h" //spy vs spy
+//#define APP_MARKER 0xCA21
+//#include "GAMES/rom_22.h" //Robocop
+//#define APP_MARKER 0xCA22
+//#include "GAMES/rom_23.h" //Solar Striker
+//#define APP_MARKER 0xCA23
+//#include "GAMES/rom_26.h" //Mortal Combat
+//#define APP_MARKER 0xCA26
+//#include "GAMES/rom_27.h" //Mortal Combat 2
+//#define APP_MARKER 0xCA27
+//#include "GAMES/rom_28.h" //Pokemon blue
+//#define APP_MARKER 0xCA28
+//#include "GAMES/rom_29.h" //Q-bert
+//#define APP_MARKER 0xCA29
+//#include "GAMES/rom_30.h" //PacMan
+//#define APP_MARKER 0xCA30
+//#include "GAMES/rom_31.h" //Adventure Island
+//#define APP_MARKER 0xCA31
+//#include "GAMES/rom_32.h" //Adventure Island II
+//#define APP_MARKER 0xCA32
+//#include "GAMES/rom_33.h" //Castlevania II - Belmont's Revenge
+//#define APP_MARKER 0xCA33
+//#include "GAMES/rom_34.h" //Chase H.Q. 
+//#define APP_MARKER 0xCA34
+//#include "GAMES/rom_35.h" //Speedy Gonzales 
+//#define APP_MARKER 0xCA35
+//#include "GAMES/rom_36.h" //Star Wars - The Empire Strikes Back 
+//#define APP_MARKER 0xCA36
+#include "GAMES/rom_37.h" //Super Mario Land 2 - 6 Golden Coins
+#define APP_MARKER 0xCA37
+//#include "GAMES/rom_38.h" //Super Off Road 
+//#define APP_MARKER 0xCA38
+//#include "GAMES/rom_39.h" //Pocket_Monsters_-_Green_Version_J_V1.0_S_patched
+//#define APP_MARKER 0xCA39
+//#include "GAMES/rom_40.h" //bomberman
+//#define APP_MARKER 0xCA40
+//#include "GAMES/rom_41.h" //super chase hq
+//#define APP_MARKER 0xCA41
+//#include "GAMES/rom_42.h" //Burai Fighter Deluxe
+//#define APP_MARKER 0xCA42
+//#include "GAMES/rom_43.h" //Navy Seals (USA, Europe)
+//#define APP_MARKER 0xCA43
+//#include "GAMES/rom_44.h" //Tamagotchi (UE) [S][!]
+//#define APP_MARKER 0xCA44
+//#include "GAMES/rom_45.h" //Tiny Toon Adventures - Babs' Big Break (E) [!]
+//#define APP_MARKER 0xCA45
+//#include "GAMES/rom_46.h" //Yoshi (U) [!]
+//#define APP_MARKER 0xCA46
+//#include "GAMES/rom_47.h" //Donkey Kong (JU) (V1.1) [S][!]
+//#define APP_MARKER 0xCA47
+//#include "GAMES/rom_48.h" //F-1 Race (World)
+//#define APP_MARKER 0xCA48
+//#include "GAMES/rom_49.h" //Final Fantasy Adventure (USA)
+//#define APP_MARKER 0xCA49
+//#include "GAMES/rom_51.h" //Game & Watch Gallery (Europe) (SGB Enhanced)
+//#define APP_MARKER 0xCA51
+//#include "GAMES/rom_52.h" //Space Invaders (U) [S][!]
+//#define APP_MARKER 0xCA52
+//#include "GAMES/rom_53.h" //Worms (Europe)
+//#define APP_MARKER 0xCA53
+//#include "GAMES/rom_54.h" //harvest_moon(USA)
+//#define APP_MARKER 0xCA54
+//#include "GAMES/rom_55.h" //Warioland (Super Marioland 3)
+//#define APP_MARKER 0xCA55
+//#include "GAMES/rom_56.h" //Final Fantasy Legend  (USA)
+//#define APP_MARKER 0xCA56 
+//#include "GAMES/rom_57.h" //Kwirk (UE)
+//#define APP_MARKER 0xCA57
+
+
+#define WRITE_DELAY 2000
+#define CART_SIZE 10000
+#define GB_ROM rom
+
+File fle;
+
+uint8_t  *cartSave;
+uint8_t  previousSoundFlag;
+uint8_t  paletteAndOffsetChangeFlag = 0;
+uint32_t timeToSave;
+bool     cartSaveFlag = 0;
+
+int32_t maxout=0;
+int32_t minch1=0, minch2=0, minch3=0, minch4=0;
+uint32_t divider=64;
+
+struct SaveStruct{
+  uint32_t appMarker = APP_MARKER;
+  bool     soundFlag = 1;
+  bool     forceRescale = 1;
+  uint8_t  paletteNo = 3;
+  uint8_t  offset_x = 16;
+  uint8_t  offset_y = 8;
+};
+
+
+SaveStruct defaultSaveStruct, realSaveStruct;
+
+struct gb_s *gb;
+enum gb_init_error_e ret;
+
+
+void writeFS(){
+    delay(100);
+    fle = LittleFS.open("/save.dat", "r+");
+    for(uint16_t i=0; i<CART_SIZE; i++) 
+      fle.write(cartSave[i]);
+    fle.close();
+};
+
+
+
+void loadFS(){
+    delay(100);
+    fle = LittleFS.open("/save.dat", "r+");
+    for(uint16_t i=0; i<CART_SIZE; i++) 
+      cartSave[i] = fle.read();
+    fle.close();
+};
+
+
+void inline __attribute__((always_inline)) IRAM_ATTR readkeys(){
+ static uint8_t nowkeys;
+  nowkeys = myESPboy.getKeys();
+  gb->direct.joypad_bits.a = (nowkeys&PAD_ACT)?0:1;
+  gb->direct.joypad_bits.b = (nowkeys&PAD_ESC)?0:1;
+  gb->direct.joypad_bits.up = (nowkeys&PAD_UP)?0:1;
+  gb->direct.joypad_bits.down = (nowkeys&PAD_DOWN)?0:1;
+  gb->direct.joypad_bits.left = (nowkeys&PAD_LEFT)?0:1;
+  gb->direct.joypad_bits.right = (nowkeys&PAD_RIGHT)?0:1;
+  gb->direct.joypad_bits.start = (nowkeys&PAD_LFT)?0:1;
+  gb->direct.joypad_bits.select = (nowkeys&PAD_RGT)?0:1;
+  if (nowkeys&PAD_LFT && nowkeys&PAD_RGT && cartSaveFlag == 0) adjustOffset();
+}
+
+
+void adjustOffset(){
+  static uint8_t nowkeys;
+  paletteAndOffsetChangeFlag = 1;
+  previousSoundFlag = realSaveStruct.soundFlag;
+  realSaveStruct.soundFlag=0;
+  while(myESPboy.getKeys()) delay(100);
+  while(1){
+    delay(200);
+    myESPboy.tft.drawString(F("Adjusting LCD"), 24, 60);
+    myESPboy.tft.drawString(F("up/down/left/right"), 8, 70);  
+    myESPboy.tft.drawString(F("press B to go back"), 9, 90);  
+    if (previousSoundFlag) myESPboy.tft.drawString(F("Sound ON "), 0, 0);
+    else myESPboy.tft.drawString(F("Sound OFF"), 0, 0);
+    myESPboy.tft.drawString(F("Palette N  "), 0, 10);
+    myESPboy.tft.drawString((String)realSaveStruct.paletteNo, 66, 10);
+    if (realSaveStruct.forceRescale) myESPboy.tft.drawString(F("Rescale ON "), 0, 20);
+    else myESPboy.tft.drawString(F("Rescale OFF"), 0, 20);
+    while(!(nowkeys = myESPboy.getKeys())) delay(50);
+    if (!realSaveStruct.forceRescale){
+      if (nowkeys&PAD_UP && realSaveStruct.offset_y>0) realSaveStruct.offset_y--;
+      if (nowkeys&PAD_DOWN && realSaveStruct.offset_y<16) realSaveStruct.offset_y++;
+      if (nowkeys&PAD_LEFT && realSaveStruct.offset_x>0) realSaveStruct.offset_x--;
+      if (nowkeys&PAD_RIGHT && realSaveStruct.offset_x<32) realSaveStruct.offset_x++;}
+    if (nowkeys&PAD_ACT) {previousSoundFlag = !previousSoundFlag;}
+    if (nowkeys&PAD_RGT) {realSaveStruct.paletteNo++; if(realSaveStruct.paletteNo==sizeof(paletteN)/sizeof(uint32_t *)) realSaveStruct.paletteNo=0;}
+    if (nowkeys&PAD_LFT) {realSaveStruct.forceRescale = !realSaveStruct.forceRescale;}
+    if (nowkeys&PAD_ESC) {break;}
+
+    gb_run_frame(gb);
+    gb_run_frame(gb);
+  }
+
+  realSaveStruct.soundFlag = previousSoundFlag;
+  saveparameters();
+  paletteAndOffsetChangeFlag = 0;
+  myESPboy.tft.fillScreen(TFT_BLACK);
+};
+
+
+uint8_t inline __attribute__((always_inline)) IRAM_ATTR gb_rom_read(struct gb_s *gb, const uint32_t addr){
+  return mmu_get_uint8(GB_ROM+addr);
+  //return pgm_read_byte(GB_ROM+addr);
+}
+
+
+uint8_t gb_cart_ram_read(struct gb_s *gb, const uint32_t addr){
+  if (addr<CART_SIZE){
+    return cartSave[addr];}
+  else {
+    //Serial.print("Read fail addr: "); Serial.println(addr);
+  }
+  return(0);
+}
+
+
+void gb_cart_ram_write(struct gb_s *gb, const uint32_t addr, const uint8_t val){
+  
+  cartSaveFlag = 1;
+  timeToSave = millis(); 
+  
+  if (addr<CART_SIZE){
+    cartSave[addr] = val;
+  }
+  else {     
+    //Serial.print("Save fail addr: "); Serial.println(addr);
+  }
+}
+
+
+void gb_error(struct gb_s *gb, const enum gb_error_e gb_err, const uint16_t val){
+	const char* gb_err_str[4] = {
+		"UNKNOWN",
+		"INVALID OPCODE",
+		"INVALID READ",
+		"INVALID WRITE"
+	};
+
+	//Serial.print(F("Error "));
+	//Serial.print(gb_err);
+	//Serial.print(F(" occurred:  "));
+	//Serial.println(gb_err >= GB_INVALID_MAX ?gb_err_str[0] : gb_err_str[gb_err]);
+	//Serial.print(F("At Address "));
+	//Serial.println(val,HEX);
+}
+
+
+  uint_fast8_t offset_xx;
+  uint_fast8_t offset_yy;
+  uint16_t uiBuff1[128] __attribute__((aligned(32)));
+  uint16_t uiBuff2[128] __attribute__((aligned(32)));
+  uint8_t uiBuff3[160];
+  uint16_t *currentBuf;
+  uint16_t *currentBuf2;
+  uint16_t *currentBuf3;
+  uint8_t prevLine=0;
+  bool flipBuf;
+  bool skippedLineFlag = 0;
+  uint8_t *pix;
+  
+
+void IRAM_ATTR lcd_draw_line(struct gb_s *gb, const uint8_t *pixels, uint_fast8_t line){
+  if (paletteAndOffsetChangeFlag) {
+     paletteNN = (uint16_t *)paletteN[realSaveStruct.paletteNo];
+     offset_xx = realSaveStruct.offset_x;
+     offset_yy = realSaveStruct.offset_y;
+   }
+
+  if(!realSaveStruct.forceRescale){
+    if (line < offset_yy || line > offset_yy+127) return;}
+  else{
+    if (!(line%8)) {memcpy(uiBuff3, pixels, 160); skippedLineFlag = 1; return;}
+    line -= line>>3;}
+  
+  if(line != prevLine+1){
+    while(nbSPI_isBusy());
+    if (!realSaveStruct.forceRescale) myESPboy.tft.setAddrWindow(0, line-offset_yy, 128, 128);
+    else  myESPboy.tft.setAddrWindow(0, line, 128, 128);}
+
+    currentBuf = flipBuf?uiBuff1:uiBuff2;
+    currentBuf2 = currentBuf;
+    currentBuf3 = &currentBuf[128];
+
+    if (!realSaveStruct.forceRescale) pix = (uint8_t *)&pixels[offset_xx];
+    else pix = (uint8_t *)&pixels[0];
+
+  if(skippedLineFlag){
+    skippedLineFlag=0;  
+    uint8_t *pixx = &uiBuff3[0];
+    while (currentBuf != currentBuf3){  
+      *currentBuf++ = paletteNN[((*pix)+(*pixx))>>1]; pix++; pixx++;
+      *currentBuf++ = paletteNN[((*pix)+(*pixx))>>1]; pix++; pixx++;
+      *currentBuf++ = paletteNN[((*pix)+(*pixx))>>1]; pix++; pixx++;
+      if(!realSaveStruct.forceRescale) {*currentBuf++ = paletteNN[((*pix)+(*pixx))>>1]; pix++; pixx++;}
+      else {*currentBuf++ = paletteNN[(pix[0]+pix[1]+pixx[0]+pixx[1])>>2]; pix+= 2; pixx+= 2;}
+      *currentBuf++ = paletteNN[((*pix)+(*pixx))>>1]; pix++; pixx++;
+      *currentBuf++ = paletteNN[((*pix)+(*pixx))>>1]; pix++; pixx++;
+      *currentBuf++ = paletteNN[((*pix)+(*pixx))>>1]; pix++; pixx++;
+      if(!realSaveStruct.forceRescale) {*currentBuf++ = paletteNN[((*pix)+(*pixx))>>1]; pix++; pixx++;}
+      else {*currentBuf++ = paletteNN[(pix[0]+pix[1]+pixx[0]+pixx[1])>>2]; pix+= 2; pixx+= 2;}
+    }
+  }
+  else{      
+    while (currentBuf != currentBuf3){
+      *currentBuf++ = paletteNN[(*pix++)];
+      *currentBuf++ = paletteNN[(*pix++)];
+      *currentBuf++ = paletteNN[(*pix++)];
+      if(!realSaveStruct.forceRescale) *currentBuf++ = paletteNN[(*pix++)];
+      else {*currentBuf++ = paletteNN[(pix[0]+pix[1])>>1]; pix+= 2;}
+      *currentBuf++ = paletteNN[(*pix++)];
+      *currentBuf++ = paletteNN[(*pix++)];
+      *currentBuf++ = paletteNN[(*pix++)];
+      if(!realSaveStruct.forceRescale) *currentBuf++ = paletteNN[(*pix++)];
+      else {*currentBuf++ = paletteNN[(pix[0]+pix[1])>>1]; pix+= 2;}
+    }
+  }
+
+    while(nbSPI_isBusy()); nbSPI_writeBytes((uint8_t*)currentBuf2, 256);   //unsafe but fast render method
+    //myESPboy.tft.pushColors(currentBuf2, 128, false); //safe but slow render method 
+
+    prevLine = line;
+    flipBuf = !flipBuf;
+}
+
+
+
+void IRAM_ATTR sound_ISR(){
+  if(realSaveStruct.soundFlag)
+    sigmaDeltaWrite(0, audio_update());
+}
+
+
+bool loadparameters(){
+  EEPROM.get(0, realSaveStruct);
+  if(realSaveStruct.appMarker != APP_MARKER){
+    EEPROM.put(0, defaultSaveStruct);
+    EEPROM.commit();
+    realSaveStruct = defaultSaveStruct;
+    return(1);
+  }
+  return(0);
+};
+
+
+void saveparameters(){
+  delay(100);
+  realSaveStruct.soundFlag = previousSoundFlag;
+  EEPROM.put(0, realSaveStruct);
+  EEPROM.commit();
+};
+
+
+void setup() {
+  //system_update_cpu_freq(SYS_CPU_160MHZ);
+    
+  //Serial.begin(115200);
+  
+  //Serial.println();
+  //Serial.println(ESP.getFreeHeap());
+
+  myESPboy.begin("GameBoy emu 2.7");
+
+//Check OTA2
+//  if (myESPboy.getKeys()&PAD_ACT || myESPboy.getKeys()&PAD_ESC) { 
+//     terminalGUIobj = new ESPboyTerminalGUI(&myESPboy.tft, &myESPboy.mcp);
+//     OTA2obj = new ESPboyOTA2(terminalGUIobj);
+//  }
+
+
+  WiFi.mode(WIFI_OFF);
+  Wire.setClock(400000L);
+  
+  myESPboy.tft.setTextColor(TFT_YELLOW, TFT_BLACK);
+  myESPboy.tft.fillScreen(TFT_BLACK);
+
+//EEPROM init (for game cart RAM)  
+  EEPROM.begin(sizeof(SaveStruct));
+
+  // File system init
+  cartSave = (uint8_t *)malloc (CART_SIZE+1);
+    
+  LittleFS.begin();
+  if (loadparameters()){
+    myESPboy.tft.drawString(F("Init file system..."), 0, 0);
+    LittleFS.format();
+    fle = LittleFS.open("/save.dat", "a");
+    for(uint16_t i=0; i<CART_SIZE; i++) fle.write(0);
+    fle.close();
+    myESPboy.tft.fillScreen(TFT_BLACK);
+    myESPboy.tft.drawString(F("<< press both side >>"), 0, 15);
+    myESPboy.tft.drawString(F("buttons for settings"), 3, 30);
+    delay(3000);
+    myESPboy.tft.fillScreen(TFT_BLACK);
+  }
+
+  loadFS();
+
+  paletteNN = (uint16_t *)paletteN[realSaveStruct.paletteNo];
+  offset_xx = realSaveStruct.offset_x;
+  offset_yy = realSaveStruct.offset_y;
+
+// init game boy emulator
+   gb = new gb_s;
+
+   ret = gb_init(gb, &gb_rom_read, &gb_cart_ram_read, &gb_cart_ram_write, &gb_error, NULL);
+
+  //if(ret != GB_INIT_NO_ERROR){
+    //Serial.print("Error: ");
+    //Serial.println(ret);}
+
+    gb_init_lcd(gb, &lcd_draw_line);
+    gb->direct.interlace = 0;
+    gb->direct.frame_skip = 1;
+  
+  sigmaDeltaSetup(0, SAMPLING_RATE);
+  sigmaDeltaAttachPin(SOUNDPIN);
+  sigmaDeltaEnable();
+  noInterrupts();
+  timer1_isr_init();
+  timer1_attachInterrupt(sound_ISR);
+  timer1_enable(TIM_DIV1, TIM_EDGE, TIM_LOOP);
+  timer1_write(80 * 1000000 / SAMPLING_RATE);//AUDIO_SAMPLE_RATE);
+  interrupts();
+
+  //Serial.println(ESP.getFreeHeap());
+
+  myESPboy.tft.setAddrWindow(0, 0, 128, 128);
+}
+
+
+
+#define FRAME_TIME 12000
+uint32_t nextScreen;
+uint8_t passGetkey;
+
+void loop() {   
+   if(passGetkey > 4){
+      readkeys();
+      divider = (maxout>>8)+1;
+      passGetkey=0;}
+   passGetkey++;
+   
+   gb_run_frame(gb);
+ 
+  if (cartSaveFlag && millis() - timeToSave > WRITE_DELAY){
+    previousSoundFlag = realSaveStruct.soundFlag;
+    realSaveStruct.soundFlag=0;
+    writeFS();
+    realSaveStruct.soundFlag = previousSoundFlag;
+    cartSaveFlag = 0;
+  }
+
+  while(nextScreen > micros());
+  nextScreen = micros() + FRAME_TIME ;
+};
