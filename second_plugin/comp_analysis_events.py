e80cd8a035ac9aea6010dbd0ff31507faa8f5cc1
November8 <d.mcfawlt@gmail.com>
2024-09-10 19:52:59 -0400

    Addeed extra stuff to sustainability evaluation

--- second_plugin/comp_analysis_events.py
+++ second_plugin/comp_analysis_events.py
@@ -10,16 +10,16 @@ DEFAULT_CONFIG_FILE = os.path.join(current_folder, 'config.ini')
 
 CONFIG_FILE_PARAMS = {
     "Deposition": {
-        "diameter": {
-            "FriendlyName":"Diameter",
-            "Unit":"mm",
-            "Default":0.4
-        },
-        "epoxy_overflow": {
-            "FriendlyName":"Epoxy Overflow",
-            "Unit":"mm",
-            "Default":0
-        },
+        # "diameter": {
+        #     "FriendlyName":"Diameter",
+        #     "Unit":"mm",
+        #     "Default":0.4
+        # },
+        # "epoxy_overflow": {
+        #     "FriendlyName":"Epoxy Overflow",
+        #     "Unit":"mm",
+        #     "Default":0
+        # },
         "deposition_feedrate": {
             "FriendlyName":"Depostion Feedrate",
             "Unit":"mm/s",
@@ -38,11 +38,11 @@ CONFIG_FILE_PARAMS = {
             "Unit":"mm/s",
             "Default":20
         },
-        "plunge_amount": {
-            "FriendlyName":"Plunge Amount",
-            "Unit":"mm",
-            "Default":0.05
-        },
+        # "plunge_amount": {
+        #     "FriendlyName":"Plunge Amount",
+        #     "Unit":"mm",
+        #     "Default":0.05
+        # },
         "engraving_wattage": {
             "FriendlyName":"Engraving Wattage",
             "Unit":"W",
@@ -105,21 +105,21 @@ CONFIG_FILE_PARAMS = {
             "Unit":"oz",
             "Default":1
         },
-        "copper_density": {
-            "FriendlyName":"Copper Density",
-            "Unit":"g/cm3",
-            "Default":8.96
-        },
-        "fiberglass_thickness": {
-            "FriendlyName":"Fiberglass Thickness",
-            "Unit":"mm",
-            "Default":1.6
-        },
-        "fiberglass_density": {
-            "FriendlyName":"Fiberglass Density",
-            "Unit":"g/cm3",
-            "Default":1.850
-        },
+        # "copper_density": {
+        #     "FriendlyName":"Copper Density",
+        #     "Unit":"g/cm3",
+        #     "Default":8.96
+        # },
+        # "fiberglass_thickness": {
+        #     "FriendlyName":"Fiberglass Thickness",
+        #     "Unit":"mm",
+        #     "Default":1.6
+        # },
+        # "fiberglass_density": {
+        #     "FriendlyName":"Fiberglass Density",
+        #     "Unit":"g/cm3",
+        #     "Default":1.850
+        # },
     },
 
     "Price": {
@@ -160,17 +160,16 @@ CONFIG_USER_PARAMS = {
         "iteration_number": {
             "FriendlyName": "Iteration Number",
             "Unit":"",
-            "Default":1
+            "Default":2
         }
     }
 }
 
-WRITE_PATH_LEN = 10
-ERASE_PATH_LEN = 10
-NEW_BOARD_PATH_LEN = 100
-
 MM2_TO_FT2 = 1.07639 * (10**-5)
 OZ_TO_G = 28.3495
+
+COPPER_DENSITY = 8.96
+FIBERGLASS_DENSITY = 1.850
 
 def format_float(num, places=2):
     return f"{round(num, places):.2f}"
@@ -341,17 +340,49 @@ class CompAnalysisDialog(AnalysisDialog):
         self.write_paths = PathParams(path_dict=write_paths)
         self.erase_edges = erase_edges
 
-        time = self.CalcTime()
-        epoxy, stencil, fr4 = self.CalcMaterial()
-        price = self.CalcPrice()
-        energy = self.CalcEnergy()
+        original_time, renewal_time, time_diff = self.CalcTime()
+        epoxy_weight, stencil_area, fr4_area, weight_diff = self.CalcMaterial()
+        original_price, renewal_price, price_diff = self.CalcPrice()
+        original_energy, renewal_energy, energy_diff = self.CalcEnergy()
 #         text = f'''
 # =====Sustainability Evaluation=====
 # Time Saved = {time}\nEpoxy Used = {epoxy}\nCopper Saved = {copper}\nFiberglass Saved = {fiberglass}\nMoney Saved = {price}\nEnergy Saved = {energy}'''
         text = f'''
 =====Sustainability Evaluation=====
-Extra Time Used = {time}\nEpoxy Used = {epoxy}\nStencil Area Used = {stencil}\nFR4 Area Saved = {fr4}\nMoney Saved = {price}\nEnergy Saved = {energy}'''
-        
+Material Saved = {weight_diff}
+    PCB Renewal Weight = {epoxy_weight}
+    New FR4 Weight = {format_float(self.original_weight)} mg
+Money Saved = {price_diff}
+    PCB Renewal Price = {renewal_price}
+    New FR4 Price = {original_price}
+Time Saved = {time_diff}
+    PCB Renewal Time = {renewal_time}
+    New FR4 Time = {original_time}
+Energy Saved = {energy_diff}
+    PCB Renewal Energy = {renewal_energy}
+    New FR4 Energy = {original_energy}
+
+========Itemized Consumption========
+Epoxy Used = {epoxy_weight}
+Stencil Area Used = {stencil_area}
+FR4 Area Saved = {fr4_area}
+
+Epoxy Price = $ {format_float(self.epoxy_price)}
+Stencil Price = $ {format_float(self.stencil_price)}
+FR4 Price = {original_price}
+
+Deposition Time = {format_float(self.deposition_time / 60)} min
+Curing Time = {format_float(self.curing_time / 60)} min
+Laser Cutting Time = {format_float(self.stencil_time / 60)} min
+Renewal Engraving Time = {format_float(self.write_time / 60)} min
+New FR4 Engraving Time = {format_float(self.original_time / 60)} min
+
+Deposition Energy = {format_float(self.deposition_energy)} J
+Curing Energy = {format_float(self.curing_energy)} J
+Laser Cutting Energy = {format_float(self.laser_energy)} J
+Renewal Engraving Energy = {format_float(self.renewal_engraving_energy)} J
+New FR4 Engraving Energy = {original_energy}
+'''
         return text
         pass
 
@@ -362,7 +393,6 @@ Extra Time Used = {time}\nEpoxy Used = {epoxy}\nStencil Area Used = {stencil}\nF
         eng_fr = float(self.file_params["Engraving"]["engraving_feedrate"])
         eng_stepdown = float(self.file_params["Engraving"]["engraving_stepdown"])
         original_len = self.new_board.total_path_length
-        eng_depth = float(self.file_params["Engraving"]["plunge_amount"])
 
         out_fr = float(self.file_params["Outline"]["outline_feedrate"])
         out_stepdown = float(self.file_params["Outline"]["outline_stepdown"])
@@ -372,6 +402,11 @@ Extra Time Used = {time}\nEpoxy Used = {epoxy}\nStencil Area Used = {stencil}\nF
         las_fr = float(self.file_params["Laser Cutter"]["laser_feedrate"])
 
         iter_num = int(self.user_params["iteration_number"])
+        groove_depth = float(self.user_params["groove_depth"])
+
+        eng_depth = groove_depth + (0.05 * (iter_num - 1))
+
+        print(f"original len = {original_len}")
 
         self.original_time = \
             ((original_len / eng_fr) * math.ceil(eng_depth / eng_stepdown)) +  ((original_out_length / out_fr) * math.ceil(board_thickness / out_stepdown))
@@ -384,37 +419,48 @@ Extra Time Used = {time}\nEpoxy Used = {epoxy}\nStencil Area Used = {stencil}\nF
         write_eng_depth = eng_depth + (0.05 * (iter_num - 1))
         write_len = self.write_paths.total_path_length
         write_out_len = self.erase_edges.cut_length
+
+        # print(f"{}")
         
         self.write_time = \
             ((write_len / eng_fr) * math.ceil(write_eng_depth / eng_stepdown)) + ((write_out_len / out_fr) * math.ceil(board_thickness / out_stepdown))
 
         renewal_time_min = (self.curing_time + self.stencil_time + self.deposition_time + self.write_time) / 60
 
-        return f"{format_float(renewal_time_min)} min - {format_float(original_time_min)} min = {format_float(renewal_time_min - original_time_min)} min"
+        return f"{format_float(original_time_min)} min", f"{format_float(renewal_time_min)}", f"{format_float(original_time_min - renewal_time_min)} min"
         pass
     
     def CalcMaterial(self):
         epoxy_density = float(self.file_params["Material"]["epoxy_density"])
         erase_area = self.erase_paths.total_path_area
         groove_depth = float(self.user_params["groove_depth"])
+        iter_num = float(self.user_params["iteration_number"])
         print(epoxy_density, erase_area, groove_depth)
-        epoxy_weight = epoxy_density * erase_area * groove_depth
+        self.epoxy_filling_depth = groove_depth + ((iter_num - 2) * 0.05)
+
+        # The 1000 is because we're converting mm3 to cm3
+        epoxy_weight = epoxy_density * erase_area * groove_depth #/ 1000 Ignoring the divide by 1000 so it'll be in miligrams
         self.epoxy_weight = epoxy_weight
         stencil_area = self.old_board.edge_area
 
         fr4_area = self.new_board.edge_area
 
-        # copper_per_ft2 = float(self.file_params["Material"]["copper_thickness"]) 
-        # fbr_thickness = float(self.file_params["Material"]["fiberglass_thickness"])
+        copper_per_ft2 = float(self.file_params["Material"]["copper_thickness"]) 
+        fbr_thickness = float(self.user_params["board_thickness"]) - 0.1
         # fbr_density = float(self.file_params["Material"]["fiberglass_density"])
 
         # original_area = self.new_board.edge.get_area_mm() * MM2_TO_FT2
 
-        # copper_weight = copper_per_ft2 * original_area * OZ_TO_G
         # fbr_weight = original_area * fbr_thickness * fbr_density
 
-        # return f"{format_float(epoxy_volume)} ml", f"{format_float(copper_weight)} g", f"{format_float(fbr_weight)} g"
-        return f"{format_float(epoxy_weight)} g", f"{format_float(stencil_area)} mm2",f"{format_float(fr4_area)} mm2"
+        copper_weight = copper_per_ft2 * (fr4_area * MM2_TO_FT2) * OZ_TO_G
+        print(fr4_area, fbr_thickness, FIBERGLASS_DENSITY, 1000)
+        fiberglass_weight = fr4_area * fbr_thickness * FIBERGLASS_DENSITY #/ 1000 # cm3 to mm3 in mg
+        # print("weights", copper_weight, fiberglass_weight)
+        
+        self.original_weight = copper_weight + fiberglass_weight
+
+        return f"{format_float(epoxy_weight)} mg", f"{format_float(stencil_area)} mm2",f"{format_float(fr4_area)} mm2", f"{format_float(self.original_weight - epoxy_weight)} mg"
         pass
 
     def CalcPrice(self):
@@ -423,9 +469,13 @@ Extra Time Used = {time}\nEpoxy Used = {epoxy}\nStencil Area Used = {stencil}\nF
         epoxy_rate = float(self.file_params["Price"]["epoxy_price"])
         fr4_rate = float(self.file_params["Price"]["fr4_price"])
         stencil_rate = float(self.file_params["Price"]["stencil_price"])
-        erase_n_write_price = (self.epoxy_weight * epoxy_rate) + (self.new_board.edge_area * stencil_rate)
+
+        self.stencil_price = (self.new_board.edge_area * stencil_rate)
+        self.epoxy_price = (self.epoxy_weight * epoxy_rate)
+
+        renewal_price = self.epoxy_price + self.stencil_price
         original_price = fr4_rate * self.new_board.edge_area
-        return f"${format_float(original_price)} - ${format_float(erase_n_write_price)} = ${format_float(original_price - erase_n_write_price)}"
+        return f"${format_float(original_price)}", f"${format_float(renewal_price)}", f"${format_float(original_price - renewal_price)}"
         pass
     
     def CalcEnergy(self):
@@ -435,8 +485,12 @@ Extra Time Used = {time}\nEpoxy Used = {epoxy}\nStencil Area Used = {stencil}\nF
         las_wtt = float(self.file_params["Laser Cutter"]["laser_wattage"])
 
         original_energy = eng_wtt * self.original_time
+        self.renewal_engraving_energy = (eng_wtt * self.write_time)
+        self.deposition_energy = (dep_wtt * self.deposition_time)
+        self.curing_energy = (heat_wtt * self.curing_time)
+        self.laser_energy = (las_wtt * self.stencil_time)
         renewal_energy = (eng_wtt * self.write_time) + (dep_wtt * self.deposition_time) + (heat_wtt * self.curing_time) + (las_wtt * self.stencil_time)
-        return f"{format_float(original_energy)} J - {format_float(renewal_energy)} J = {format_float(original_energy - renewal_energy)} J"
+        return f"{format_float(original_energy)} J", f"{format_float(renewal_energy)} J", f"{format_float(original_energy - renewal_energy)} J"
         pass
 
     def OKClicked(self, event):
